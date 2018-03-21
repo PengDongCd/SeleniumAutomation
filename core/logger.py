c4b459c76ef4133ca8ff751eea56ed19f39872ba
Paddy Dong <paddy.dong@activenetwork.com>
2018-03-21 17:35:24 +0800

    update logger to support log in file and print in console

--- core/logger.py
+++ core/logger.py
@@ -2,6 +2,8 @@ import logging
 import time
 import os
 
+import sys
+
 
 class TestLogger:
     def __init__(self, test_script_name):
@@ -10,7 +12,8 @@ class TestLogger:
             os.makedirs(log_file__folder_path)
         time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         test_log_file_path = os.path.join(log_file__folder_path,str(test_script_name)+' '+time_str+'.log')
-        logging.basicConfig(filename=test_log_file_path, level=logging.INFO,
+        logging.basicConfig(level=logging.INFO,
                                 format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                                 datefmt='%Y-%m-%d %H:%M:%S',
-                                filemode='w')
+                                handlers=[logging.FileHandler(test_log_file_path),logging.StreamHandler()]
+                                )
