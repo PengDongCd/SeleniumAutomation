501250bdd108ea64b36d5c042504b98fde8c29b6
Paddy Dong <paddy.dong@activenetwork.com>
2018-03-26 17:43:51 +0800

    update webdriver method

--- core/webdriver_base.py
+++ core/webdriver_base.py
@@ -1,11 +1,15 @@
 import selenium
+import time
 from selenium import webdriver
 import logging
 import os
 
-DOWNLOAD_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'outputs/download')
-if os.path.exists(DOWNLOAD_PATH):
+DOWNLOAD_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'outputs', 'download')
+if not os.path.exists(DOWNLOAD_PATH):
     os.mkdir(DOWNLOAD_PATH)
+SCREENSHOOT_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'outputs', 'screenshots')
+if not os.path.exists(SCREENSHOOT_PATH):
+    os.mkdir(SCREENSHOOT_PATH)
 
 class TestWebDriver:
     def __init__(self, browser_name):
@@ -27,6 +31,9 @@ class TestWebDriver:
             chrome_opts.add_argument('--disable-popup-blocking')
             chrome_opts.add_argument('--ignore-certificate-errors')
             chrome_opts.add_argument('--test-type')
+            chrome_opts.add_argument('--headless')
+            chrome_opts.add_argument('--disable-gpu')
+            #chrome_opts.add_argument('--remote-debugging-port=9222')
             self.driver = webdriver.Chrome(chrome_options=chrome_opts)
             logging.info("Chrome started!")
         else:
@@ -55,6 +62,8 @@ class TestWebDriver:
                 return self.driver.find_element_by_partial_link_text(element.find_value)
             elif element.find_method == 'css_selector':
                 return self.driver.find_element_by_css_selector(element.find_value)
+            else:
+                logging.error("the find method {find_method}is not correct!".format(find_method=element.find_method))
         except selenium.common.exceptions.NoSuchElementException:
             return None
 
@@ -83,4 +92,10 @@ class TestWebDriver:
         self.driver.get(url)
 
     def refresh_page(self):
-        pass
+        self.driver.refresh()
+        logging.info("Current page is refreshed!")
+
+    def save_screenshot(self):
+        time_str = time.strftime("%Y%m%d_%H%M%S", time.localtime())
+        file_name = os.path.join(SCREENSHOOT_PATH, 'screenshot_at_'+ time_str + '.png')
+        self.driver.save_screenshot(file_name)
